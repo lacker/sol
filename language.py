780a5f9b042cc227a037e1587e8617a9f7b3aaaa
Kevin Lacker <lacker@gmail.com>
2024-08-10 08:49:00 -0700

    quote

--- language.py
+++ language.py
@@ -2,4 +2,6 @@ def eval_list_form(code):
     if type(code) != list:
         return code
     fn, args = code[0], code[1:]
+    if fn == "quote":
+        return args
     raise Exception("TODO: handle lists")
